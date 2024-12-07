20d29f812bef54a084d4a3b18a936820892b6b6c
Maria <bmdmaria.mdb@gmail.com>
2024-12-07 13:35:14 +0200

    Am adaugat atribute tabela User

--- Ad01/models.py
+++ Ad01/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+    type = models.CharField(max_length=32, default="Full Time")
+    salary = models.IntegerField(null=True, blank=True)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=64, unique=True)
+    
